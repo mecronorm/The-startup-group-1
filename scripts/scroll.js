af5c7efd318145e85900e9ef2e6ee812a8547fd4
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-18 11:49:53 +0200

    Add support for navbar scroll on window resize

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,28 +2,36 @@ addScrollListener();
 
 function addScrollListener() {
     window.addEventListener('scroll', () => {
-        const verticalScrollPx = window.scrollY;
-        const windowWidth = window.innerWidth;
-        
-        var nav = document.body.getElementsByTagName('nav')[0];
-        var ul = document.body.getElementsByTagName('ul')[0];
-
-        if (verticalScrollPx > 1) {
-            nav.classList.add('fixed');
-            nav.classList.add('top-0');
-            nav.classList.add('bg-blue-200');
-            nav.classList.remove('p-10');
-            nav.classList.add('p-2');
-            ul.classList.add('m-auto');
-            nav.classList.remove('bg-hero');
-        } else if (windowWidth > 1024) {
-            nav.classList.remove('fixed');
-            nav.classList.remove('top-0');
-            nav.classList.remove('bg-blue-200');
-            nav.classList.add('p-10');
-            nav.classList.remove('p-2');
-            ul.classList.remove('m-auto');
-            nav.classList.add('bg-hero');
-        }
+        changeNavclasses();
     });
+
+    window.addEventListener('resize', () => {
+        changeNavclasses();
+    });
+}
+
+function changeNavclasses() {
+    const verticalScrollPx = window.scrollY;
+    const windowWidth = window.innerWidth;
+    
+    var nav = document.body.getElementsByTagName('nav')[0];
+    var ul = document.body.getElementsByTagName('ul')[0];
+
+    if (verticalScrollPx > 1 || windowWidth < 1024) {
+        nav.classList.add('fixed');
+        nav.classList.add('top-0');
+        nav.classList.add('bg-blue-200');
+        nav.classList.remove('p-10');
+        nav.classList.add('p-2');
+        ul.classList.add('m-auto');
+        nav.classList.remove('bg-hero');
+    } else {
+        nav.classList.remove('fixed');
+        nav.classList.remove('top-0');
+        nav.classList.remove('bg-blue-200');
+        nav.classList.add('p-10');
+        nav.classList.remove('p-2');
+        ul.classList.remove('m-auto');
+        nav.classList.add('bg-hero');
+    }
 }
