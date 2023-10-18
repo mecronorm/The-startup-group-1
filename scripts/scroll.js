c4460f1d0d8e62486563bf3db10f7ddb5d639074
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-10-18 18:55:44 +0200

    Add shadow on scroll for all nav, remove parallax header on small screen

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,4 +1,5 @@
 var homeNav = document.body.getElementsByClassName('homeNav')[0];
+var nav = document.body.getElementsByTagName('nav')[0];
 var homeUl = document.body.getElementsByClassName('homeUl')[0];
 
 addScrollListener();
@@ -36,6 +37,9 @@ function changeNavclasses() {
     const verticalScrollPx = window.scrollY;
     const windowWidth = window.innerWidth;
     
+    if( verticalScrollPx > 1) nav.classList.add('shadow');
+    else nav.classList.remove('shadow');
+
     if (homeNav != null && homeUl != null) {
         if (verticalScrollPx > 1 || windowWidth < 1024) {
             homeNav.classList.add('bg-blue-200');
@@ -43,7 +47,6 @@ function changeNavclasses() {
             homeNav.classList.add('p-2');
             homeUl.classList.add('m-auto');
             homeNav.classList.remove('bg-hero');
-            homeNav.classList.add('shadow');
         } else {
              homeNav.classList.remove('bg-blue-200');
              homeNav.classList.add('bg-white');
@@ -51,7 +54,6 @@ function changeNavclasses() {
              homeNav.classList.remove('p-2');
              homeUl.classList.remove('m-auto');
              homeNav.classList.add('bg-hero');
-             homeNav.classList.remove('shadow');
         }
     }
 }
